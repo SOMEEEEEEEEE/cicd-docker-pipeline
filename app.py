39dbb6d3e42785c2928abf1d644cc3cd9a59c431
SOMEEEEEEEEE <1002485476@qq.com>
2026-03-22 16:17:20 +0000

    feat: add multi-instance Flask deployment with Nginx load balancing

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import socket
 import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return f"Hello, CI/CD! | {datetime.datetime.now()}"
+        return f"Served by {socket.gethostname()} | {datetime.datetime.now()}"
 
 if __name__ == "__main__":
         app.run(host = "0.0.0.0", port=8000)
